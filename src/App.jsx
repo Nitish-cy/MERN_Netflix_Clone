a63cb7b17c0250771b49268da6c23626734f9ee2
Nitish.mishra1 <Nitish.mishra1@RBLLNHO0424>
2023-01-16 11:58:10 +0530

    Final commit for UI (Nitish Mishra)

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
-//import Home from './home/home';
+import Home from './home/home';
 import "./App.scss"
+import Login from './components/pages/login/Login';
+import Register from './components/pages/register/Register';
 import Watch from './components/pages/watch/Watch';
 
  const App=()=> {
 
   return (
-    <Watch></Watch>
-    // <Home></Home>
+    //<Login></Login>
+    //<Register></Register>
+    
+    //<Watch></Watch>
+    <Home></Home>
   )
 };
 export default App;
